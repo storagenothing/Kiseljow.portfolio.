efc24699fdf0570bd743bd53e97e6f694862419a
storagenothing <115945740+storagenothing@users.noreply.github.com>
2023-02-13 18:29:59 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/NAV CLOSE.svg";
+    } else {
+        navBtnImg.src = "./img/NAV.svg";
+    }
+}
+
+AOS.init({
+    once: true
+});
